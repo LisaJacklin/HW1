50d9f67ccf10c6781479797257bb2b71d7584cda
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-08-30 18:31:55 -0800

    hello code adjusted and working

---
+++ HW0/HW0hello/HW0hello.cpp
@@ -0,0 +1,14 @@
+// HW0hello.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Lisa Jacklin
+// CS 201
+//8/28/2021
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, my name's Lisa!\n";
+}
+
+
