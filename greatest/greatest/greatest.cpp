d1ba55e3bb4616f648df4c9b6d0bb8a545c70582
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-09 14:10:20 -0800

    numbers are printed and accepted. issues with ending and negatives

--- greatest/greatest/greatest.cpp
+++ greatest/greatest/greatest.cpp
@@ -6,15 +6,31 @@
 //
 
 #include <iostream>
+using std::cin;
 
 int number;
 
 int main()
 {
-    std::cout << " Enter a Sequence of positive integers, ending with zero\n";
-    std::cin >> number;
-    std::cout << "Enter a positive integer ( 0 to end): ";
-    std::cout << number;
+    number = 1;
+
+    while (number != 0) {
+        std::cout << " Enter a Sequence of positive integers, ending with zero\n";
+        std::cin >> number;
+        std::cout << "Enter a positive integer ( 0 to end): ";
+        std::cout << number;
+        std::cout << "\n";
+
+       
+    }
+    if (number = 0)
+    {
+        std::cout << "The largest integer entered is: ";
+        std::cout << number;
+
+    }
+
+
 
 
 
