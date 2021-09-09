df4d3ea94a2e9fa392a88a6aef8387f5ba87214d
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-09 15:16:59 -0800

    int end added

--- greatest/greatest/greatest.cpp
+++ greatest/greatest/greatest.cpp
@@ -9,29 +9,30 @@
 using std::cin;
 
 int number;
+int end;
 
 int main()
 {
     number = 1;
-  
+    end = 0;
 
-    while (number != 0) {
+    while (number != end) {
         std::cout << " Enter a Sequence of positive integers.To end, enter zero: \n";
         std::cin >> number;
 
-        if (number >0 ){
+        if (number > end ){
         std::cout << "Enter a positive integer ( 0 to end): ";
         std::cout << number;
         std::cout << "\n";
         }
 
-        if (number < 0) {
+        if (number < end) {
             std::cout << "Please insert a positive integer: ";
                 std::cin >> number;
         }
-
+        
     }
-    while (number = 0) {
+    if (number = end) {
         std::cout << "The largest integer entered is: ";
         std::cout << number;
     }
