d12daab85606cf52b82a77299f10aeb2aae57a56
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-09 10:57:31 -0800

    diamond working and printing

--- diamond/diamond.cpp
+++ diamond/diamond.cpp
@@ -7,13 +7,52 @@
 #include <iostream>
 
 int number;
+//use for one direction of the diamond
+int i; 
+//used for the other direction
+int j;
+
 
 int main()
 {
-	std::cout << "Please enter a possitive integer";
+    // gives the number to make the diamond's thickest line
+	std::cout << "Please enter a possitive integer: ";
 		std::cin >> number;
-	
-	std::cout << "the number given was";
+	//diamond number printed
+	std::cout << "the number given was ";
+	std::cout << number;
+    std::cout << "\n";
+
+    int space = number - 1;
+
+    for (  i = 0; i < number; i++)
+    {
+     
+        for (  j = 0; j < space; j++)
+            std::cout << " ";
+
+        for (  j = 0; j <= i; j++)
+            std::cout << "* ";
+
+        std::cout << "\n";
+        space--;
+    }
+
+    space = 0;
+
+ 
+    for (  i = number; i > 0; i--)
+    {
+  
+        for (  j = 0; j < space; j++)
+            std::cout << " ";
 
 
+        for (  j = 0; j < i; j++)
+            std::cout << "* ";
+
+        std::cout << "\n";
+        space++;
+    }
 }
+
